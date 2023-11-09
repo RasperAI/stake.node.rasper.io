c96d8700e63e4cbac06387075b905816328aeb02
Amrit Pal <lucky723074@gmail.com>
2023-11-09 15:08:33 +0530

    fix try catch

--- index.js
+++ index.js
@@ -32,29 +32,34 @@ const main = async () => {
     await contract.on('Transfer', async function (from, to, value, event) {
         console.log(from, to, value, event.log.transactionHash);
 
-        const wallet = await walletModel.findOne({wallet_address: to});
-        if (wallet) {
-            console.log("db wallet ", wallet);
-            provider.waitForTransaction(event.log.transactionHash).then(async (receipt) => {
-                console.log('Transaction Mined: ' + receipt.hash);
-                console.log(receipt);
-
-                const response = await fetch(process.env.MLM_URL, {
-                    method: 'POST',
-                    body: JSON.stringify({
-                        user_id: wallet.user_id,
-                        transaction_hash: event.log.transactionHash,
-                        amount: formatUnits(value)
-                    })
-                })
-                const data = await response.json();
-                if (data) {
-                    console.log("mlm response ->", data.message);
-                }
-
-            });
+        try {
+            const wallet = await walletModel.findOne({wallet_address: to});
+            if (wallet) {
+                console.log("db wallet ", wallet);
+                provider.waitForTransaction(event.log.transactionHash).then(async (receipt) => {
+                    console.log('Transaction Mined: ' + receipt.hash);
+                    console.log(receipt);
+                    try {
+                        const response = await fetch(process.env.MLM_URL, {
+                            method: 'POST',
+                            body: JSON.stringify({
+                                user_id: wallet.user_id,
+                                transaction_hash: event.log.transactionHash,
+                                amount: formatUnits(value)
+                            })
+                        })
+                        const data = await response.json();
+                        if (data) {
+                            console.log("mlm response ->", data.message);
+                        }
+                    }catch (e){
+                        console.log(e);
+                    }
+                });
+            }
+        }catch (e){
+            console.log(e);
         }
-
     })
 
 }
